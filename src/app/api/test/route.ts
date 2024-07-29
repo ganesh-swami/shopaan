f442d6a8b0f316195dcb76a2848e3ff76b8991f0
ganesh-swami <ganeshswami018@gmail.com>
2024-07-29 17:59:35 +0530

    update api routes

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,6 +1,7 @@
 
 import { auth } from "@/auth"
-import { NextApiRequest, NextApiResponse } from "next"
+import { type NextRequest, type NextResponse } from 'next/server'
+
  
 // export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
@@ -10,10 +11,12 @@ import { NextApiRequest, NextApiResponse } from "next"
 // }
 
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const session = await auth(req, res)
-  if (session) return res.json("Success")
-  return res.status(401).json("You must be logged in.");
+export async function POST(req: NextRequest, res: NextResponse) {
+  //const session = await auth(req, res)
+  //if (session) return res.json("Success")
+  return new Response('Hello, Next.js!', {
+    status: 200
+  })
   // const data = await res.json()
  
   // return Response.json({ data })
